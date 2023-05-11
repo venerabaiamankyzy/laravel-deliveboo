3cc6078eac582b80c542765f798f1d71b3e01e51
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-05-11 17:23:26 +0200

    Added type seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RestaurantSeeder::class,
             DishSeeder::class,
+            OrderSeeder::class,
+            TypeSeeder::class,
         ]);
     }
 }
--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Type;
 use Illuminate\Database\Seeder;
 
 class TypeSeeder extends Seeder
@@ -14,6 +15,14 @@ class TypeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $types_list = ['Italiano', 'Americano', 'Cinese', 'Giapponese', 'Messicano', 'Indiano', 'Pesce', 'Carne', 'Pizza', 'Sushi', 'Vegana'];
+
+        foreach ($types_list as $type) {
+            $new_type = new Type;
+            $new_type->name = $type;
+            // $new_type->icon = '';
+            $new_type->save();
+        }
+        
     }
 }
