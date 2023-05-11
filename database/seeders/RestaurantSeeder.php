1926696dfa6866277aeaa7fa1c4c192dd93256a8
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-05-11 17:06:30 +0200

    Added restaurant seeder

--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -16,6 +16,15 @@ class RestaurantSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        
+        for($i = 0; $i < 20; $i++) {
+            $restaurant = new Restaurant;
+            $restaurant->name = $faker->company();
+            $restaurant->address = $faker->streetAddress();
+            $restaurant->vat_number = $faker->numberBetween(10000000000, 99999999999);
+            $restaurant->phone_number = '3746578985';
+            $restaurant->description = $faker->paragraph();
+            $restaurant->photo = 'https://picsum.photos/500/300';
+            $restaurant->save();
+        }
     }
 }
