7fe6fb2d1341e17365e312aeb6f38fa237ec6ade
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-05-11 17:48:40 +0200

    Added user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User;
+        $user->name = 'Admin';
+        $user->email = 'admin@admin.com';
+        $user->password = bcrypt('password');
+        $user->save();
+    }
+}
