6206b621b7e14f4d2670deda4b4c7ae0d149a225
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-05-11 16:43:12 +0200

    Created orders migration

---
+++ database/migrations/2023_05_11_143603_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('customer_name');
+            $table->string('customer_surname');
+            $table->string('customer_mail');
+            $table->string('customer_phone_number', 15);
+            $table->string('customer_address');
+            $table->float('total_amount', 6, 2);
+            $table->boolean('status');
+            $table->text('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
