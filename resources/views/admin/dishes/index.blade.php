996f78ca0a961aed56024d07f145f0e5c591c217
GioGio4 <g.ceccobelli@outlook.it>
2023-05-16 17:39:34 +0200

    Fix modal delete dish

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -41,8 +41,10 @@
                                 class="bi bi-box-arrow-in-up-right btn btn-link p-0 border-0"></a>
                             <a href="{{ route('admin.dishes.edit', $dish->id) }}"
                                 class="bi bi-brush btn btn-link p-0 border-0 ms-1"></a>
-                            <a type="button" class="btn btn-link p-0 border-0 ms-3"><i
-                                    class="bi bi-trash text-danger"></i></a>
+
+                            <button class="bi bi-trash3 text-danger btn-icon" data-bs-toggle="modal"
+                                data-bs-target="#delete-modal-{{ $dish->id }}">
+                            </button>
                         </td>
                     </tr>
                 @empty
