d82a8318bc145f12fad5b126eb990c671193a362
Venera Baiaman kyzy <venerabayaman@gmail.com>
2023-05-23 17:22:26 +0200

    add variables

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-md  bg-white shadow-sm">
+<nav class="navbar navbar-expand-md shadow-sm">
   <div class="container">
     <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
       <div class="logo_laravel">
