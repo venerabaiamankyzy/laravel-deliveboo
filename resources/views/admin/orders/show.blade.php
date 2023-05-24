34d5c5946254c90a8dd6aaec139eb7f217b5604c
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-05-24 18:05:11 +0200

    Fixed navbar link + order show

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -4,54 +4,84 @@
 
 
 @section('content')
-    <div class="container">
-        <div class="row mt-2 gx-4 gy-4 gy-lg-0">
+  <div class="container" style="height: 80vh;">
+    <div class="row mt-2 gx-4 gy-4 gy-lg-0">
+      <!-- Box form dati utente -->
+      <div class="col-12 col-lg-6">
+        <div class="form-content border rounded-3 h-100">
+          <div class="form-title p-3">
+            <h3 class="m-0">Dati cliente</h3>
+          </div>
 
-            <!-- Box dati cliente (ordine) -->
-            <div class="col-12 col-lg-6">
-                <div class="form-content border h-100 rounded-3">
-                    <div class="form-title p-3">
-                        <h3 class="m-0">Dati Ordine</h3>
-                        <hr class="m-0" />
-                        <p class="bold">ID:{{ $order->id }} </p>
-                        <p class="bold">Nome e Cognome:{{ $order->customer_name }} {{ $order->customer_surname }}</p>
-                        <p class="bold">Email:{{ $order->customer_mail }} </p>
-                        <p class="bold">Telefono:{{ $order->customer_phone_number }} </p>
-                        <p class="bold">Indirizzo:{{ $order->customer_address }} </p>
-                        <p class="bold">Totale ordine:€ {{ number_format((float) $order->total_amount, 2, '.', '') }} </p>
-                        <p class="bold">Data ordine:{{ $order->created_at }} </p>
-                        <p class="bold">Status:{{ $order->status }} </p>
-                    </div>
+          <hr class="m-0" />
 
-
-
-                    <!-- Box riepilogo ordine (piatti) -->
-                    <div class="col-12 col-lg-6">
-                        <div class="order-items-content d-flex flex-column p-3 gap-3">
-                            @foreach ($order->dishes as $dish)
-                                <div class="card">
-                                    <div class="row card-row">
-                                        <div class="col-8 h-100 ps-0">
-                                            <div class="card-body d-flex align-items-center h-100 ps-0">
-                                                <div class="card-details">
-                                                    <h5 class="card-title m-0">{{ $dish->name }}</h5>
-                                                    <p class="card-text m-0">{{ $dish->pivot->quantity }} </p>
-                                                    <span class="text-primary fw-bold me-2 d-block d-sm-none"></span>
-                                                </div>
-
-                                                <div class="item-price-delete d-flex align-items-center ms-auto">
-                                                    <span class="text-primary fw-bold me-2 d-none d-sm-block"></span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            @endforeach
-                        </div>
-                    </div>
-                </div>
-
+          <div class="p-3">
+            <div class="mb-3">
+              <span class="fw-bold">ID: </span>
+              <span>{{ $order->id }}</span>
             </div>
+            <div class="mb-3">
+              <span class="fw-bold">Nome e cognome: </span>
+              <span>{{ $order->customer_name }} {{ $order->customer_surname }}</span>
+            </div>
+            <div class="mb-3">
+              <span class="fw-bold">Email: </span>
+              <span>{{ $order->customer_mail }}</span>
+            </div>
+            <div class="mb-3">
+              <span class="fw-bold">Telefono: </span>
+              <span>{{ $order->customer_phone_number }}</span>
+            </div>
+            <div class="mb-3">
+              <span class="fw-bold">Indirizzo: </span>
+              <span>{{ $order->customer_address }}</span>
+            </div>
+            <div class="mb-3">
+              <span class="fw-bold">Totale ordine: </span>
+              <span>€ {{ number_format((float) $order->total_amount, 2, '.', '') }}</span>
+            </div>
+            <div class="mb-3">
+              <span class="fw-bold">Data ordine: </span>
+              <span>{{ $order->created_at }}</span>
+            </div>
+            <div class="mb-3">
+              <span class="fw-bold">Status: </span>
+              <span>{{ $order->status }}</span>
+            </div>
+            <div class="mb-3">
+              <span class="fw-bold">Note: </span>
+              <span style="line-break: anywhere;">{{ $order->note }}</span>
+            </div>
+          </div>
         </div>
+      </div>
+
+      <!-- Box prodotti nel carrello -->
+      <div class="col-12 col-lg-6">
+        <div class="form-content border h-100 rounded-3">
+          <div class="form-title p-3">
+            <h3 class="m-0">Riepilogo ordine</h3>
+          </div>
+
+          <hr class="m-0" />
+
+          <div class="order-items-content d-flex flex-column p-3 gap-3">
+            <table class="table table-striped mt-2 border border-dark">
+              <thead>
+                <tr>
+                  <th scope="col">Piatto</th>
+                  <th scope="col">Quantità</th>
+                </tr>
+              </thead>
+              @foreach ($order->dishes as $dish)
+                <tr>
+                  <td>{{ $dish->name }}</td>
+                  <td>{{ $dish->pivot->quantity }}</td>
+                </tr>
+              @endforeach
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
-@endsection
+  @endsection
--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -1,64 +1,66 @@
 <nav class="navbar navbar-expand-md shadow-sm">
-    <div class="container">
-        <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
-            <div class="logo_laravel">
-                <img src="{{ asset('./img/logo-white.svg') }}" alt="" class="img-fluid">
-            </div>
+  <div class="container">
+    <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
+      <div class="logo_laravel">
+        <img src="{{ asset('./img/logo-white.svg') }}" alt="" class="img-fluid">
+      </div>
 
-        </a>
+    </a>
 
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav me-auto navbar-dark ">
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/') }}">{{ __('Home') }}</a>
-                </li>
-            </ul>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <!-- Left Side Of Navbar -->
+      <ul class="navbar-nav me-auto navbar-dark ">
+        <li class="nav-item">
+          <a class="nav-link" href="{{ url('/') }}">{{ __('Home') }}</a>
+        </li>
+      </ul>
 
-            <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav  navbar-dark ml-auto">
-                <!-- Authentication Links -->
-                @guest
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                    </li>
-                    @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                    @endif
-                @else
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('admin.dishes.index') }}">{{ __('Piatti') }}</a>
-                        <a class="nav-link" href="{{ route('admin.orders.index') }}">{{ __('Ordini') }}</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }}
-                        </a>
+      <!-- Right Side Of Navbar -->
+      <ul class="navbar-nav  navbar-dark ml-auto">
+        <!-- Authentication Links -->
+        @guest
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+          </li>
+          @if (Route::has('register'))
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+            </li>
+          @endif
+        @else
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('admin.dishes.index') }}">{{ __('Piatti') }}</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('admin.orders.index') }}">{{ __('Ordini') }}</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+              data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+              {{ Auth::user()->name }}
+            </a>
 
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ url('dashboard') }}">{{ __('Dashboard') }}</a>
-                            <a class="dropdown-item" href="{{ url('profile') }}">{{ __('Profile') }}</a>
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="{{ url('dashboard') }}">{{ __('Dashboard') }}</a>
+              <a class="dropdown-item" href="{{ url('profile') }}">{{ __('Profile') }}</a>
+              <a class="dropdown-item" href="{{ route('logout') }}"
+                onclick="event.preventDefault();
                                                  document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
+                {{ __('Logout') }}
+              </a>
 
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                @endguest
-            </ul>
-        </div>
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+              </form>
+            </div>
+          </li>
+        @endguest
+      </ul>
     </div>
+  </div>
 </nav>
