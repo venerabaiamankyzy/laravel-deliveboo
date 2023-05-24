b12914151da9e27626f158dfb8246b61dece773a
GioGio4 <g.ceccobelli@outlook.it>
2023-05-24 16:45:10 +0200

    Add  Order index

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -4,16 +4,40 @@
 
 
 @section('content')
-  <div class="container">
+    <div class="container">
 
-    <ul>
-      @foreach ($orders as $order)
-        @foreach ($order->dishes as $dish)
-          @dd($dish->pivot->quantity)
-        @endforeach
-        <li>{{ $order->customer_name }} , {{ $order->customer_surname }}, €{{ $order->dishes }}</li>
-      @endforeach
-    </ul>
+        <table class="table table-striped mt-2 border border-dark">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Nome e Cognome</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Telefono</th>
+                    <th scope="col">Indirizzo</th>
+                    <th scope="col">Totale ordine</th>
+                    <th scope="col">Data ordine</th>
+                    <th scope="col">Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($orders as $order)
+                    <tr>
+                        <th scope="row">{{ $order->id }}</th>
+                        <td>{{ $order->customer_name }} {{ $order->customer_surname }}</td>
+                        <td>{{ $order->customer_mail }}</td>
+                        <td>{{ $order->customer_phone_number }}</td>
+                        <td>{{ $order->customer_address }}</td>
+                        <td>€ {{ $order->total_amount }}</td>
+                        <td>{{ $order->created_at }}</td>
+                        <td>{{ $order->status }}</td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td class="text-center" colspan="7">Nessun Ordine</td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
 
-  </div>
+    </div>
 @endsection
