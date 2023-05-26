c563ea0851e2b252cd74d5662a562778eff9a2d4
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-05-26 19:51:23 +0200

    Added earnings chart

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use Carbon\Carbon;
 use App\Models\Order;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
@@ -22,8 +23,17 @@ class DashboardController extends Controller
 
         // Ordina il conteggio degli ordini per mese
         $orderCountPerMonth = $orderCountPerMonth->sortKeys();
+
+        $totalAmountPerMonth = Order::select(DB::raw('DATE_FORMAT(created_at, "%m/%Y") AS month'), DB::raw('SUM(total_amount) AS total_amount'))
+        ->where('restaurant_id', Auth::id())
+        ->groupBy('month')
+        ->orderBy('month')
+        ->pluck('total_amount', 'month')
+        ->toArray();
+
+
         
         // Passa i dati al tuo view
-        return view('dashboard', compact('orderCountPerMonth'));
+        return view('dashboard', compact('orderCountPerMonth', 'totalAmountPerMonth'));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,19 @@
 @extends('layouts.app')
 
+@section('title', 'Dashboard')
+
 @section('content')
-  <div class="container">
-    <h2 class="fs-4 text-secondary my-4">
-      {{ __('Dashboard') }}
-    </h2>
-    <div class="row justify-content-center">
-      <div class="col-6">
-        <div>
-          <canvas id="myChart"></canvas>
-        </div>
+  <div class="row g-4">
+    <div class="col-12 col-lg-6">
+      <div class="chart-content border rounded-2 p-3">
+        <h4 class="text-center">Ordini</h4>
+        <canvas id="orderChart"></canvas>
+      </div>
+    </div>
+    <div class="col-12 col-lg-6">
+      <div class="chart-content border rounded-2 p-3">
+        <h4 class="text-center">Guadagno</h4>
+        <canvas id="totalAmountChart"></canvas>
       </div>
     </div>
   </div>
@@ -18,6 +22,7 @@
   <script>
     // Recupera i dati del grafico dal tuo controller o da altre fonti
     var orderCountPerMonth = {!! $orderCountPerMonth !!};
+    var totalAmountPerMonth = {!! json_encode($totalAmountPerMonth) !!};
 
     // Crea l'array di etichette dei mesi
     var months = Object.keys(orderCountPerMonth);
@@ -27,18 +32,51 @@
       return orderCountPerMonth[month];
     });
 
-    // Crea il grafico utilizzando Chart.js
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var myChart = new Chart(ctx, {
+    // Crea l'array di dati del guadagno totale per mese
+    var totalAmounts = months.map(function(month) {
+      return totalAmountPerMonth[month];
+    });
+
+    // Crea il primo grafico per il conteggio degli ordini
+    var orderChart = new Chart(document.getElementById('orderChart').getContext('2d'), {
       type: 'bar',
       data: {
         labels: months,
         datasets: [{
           label: 'Ordini',
           data: orderCounts,
-          backgroundColor: 'rgba(255, 159, 64, 0.4)',
+          backgroundColor: 'rgba(255, 159, 64, 0.5)',
           borderColor: 'rgb(255, 159, 64)',
-          borderWidth: 1
+          borderWidth: 2
+        }]
+      },
+      options: {
+        plugins: {
+          legend: {
+            display: false
+          }
+        },
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true,
+            ticks: {
+              stepSize: 1
+            },
+          }
+        }
+      }
+    });
+
+    // Crea il secondo grafico per il guadagno totale
+    var totalAmountChart = new Chart(document.getElementById('totalAmountChart').getContext('2d'), {
+      type: 'line',
+      data: {
+        labels: months,
+        datasets: [{
+          label: 'Guadagno',
+          data: totalAmounts,
+          borderColor: 'rgb(75, 192, 192)',
         }]
       },
       options: {
@@ -47,7 +85,23 @@
           y: {
             beginAtZero: true,
             ticks: {
-              precision: 0
+              callback: function(value) {
+                return '€' + value.toLocaleString('it-IT');
+              }
+            }
+          }
+        },
+        plugins: {
+          legend: {
+            display: false
+          },
+          tooltip: {
+            callbacks: {
+              label: function(context) {
+                var value = context.dataset.data[context.dataIndex];
+                var formattedValue = '€' + value.toLocaleString('it-IT');
+                return 'Guadagno: ' + formattedValue;
+              },
             }
           }
         }
