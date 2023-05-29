3bc2e855372fbe7dbd59c5d631ea9667e5222fac
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-05-29 16:53:42 +0200

    Adjusted order seeder

--- database/seeders/DishOrderSeeder.php
+++ database/seeders/DishOrderSeeder.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Faker\Generator as Faker;
 use App\Models\Order;
 use App\Models\Dish;
+use Illuminate\Support\Arr;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class DishOrderSeeder extends Seeder
@@ -17,11 +19,33 @@ class DishOrderSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        $dishes = Dish::all()->pluck('id')->toArray();
         $orders = Order::all();
 
         foreach ($orders as $order) {
-            $order->dishes()->attach($faker->randomElements($dishes, random_int(1, count($dishes))));
+            $restaurantId = $order->restaurant_id;
+            $dishes = Dish::where('restaurant_id', $restaurantId)->get();
+
+            $dishesWithQuantities = Arr::random($dishes->toArray(), random_int(1, 5));
+
+            $totalAmount = 0;
+
+            foreach ($dishesWithQuantities as $dish) {
+                $quantity = $faker->numberBetween(2, 5);
+                $totalAmount += $dish['price'] * $quantity;
+
+                $order->dishes()->attach($dish['id'], [
+                    'quantity' => $quantity
+                ]);
+            }
+
+            $order->total_amount = $totalAmount;
+
+            // Imposta created_at con un mese casuale da febbraio a maggio
+            $randomMonth = $faker->numberBetween(2, 5);
+            $customCreatedAt = Carbon::create(null, $randomMonth, null, 0, 0, 0);
+            $order->created_at = $customCreatedAt;
+
+            $order->save();
         }
     }
 }
--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Faker\Generator as Faker;
+use Faker\Factory as FakerFactory;
 use App\Models\Order;
 use App\Models\Restaurant;
 use Illuminate\Database\Seeder;
@@ -17,20 +18,21 @@ class OrderSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $faker = FakerFactory::create('it_IT');
+
         $restaurants = Restaurant::all()->pluck('id')->toArray();
 
         foreach ($restaurants as $restaurant) {
-            for($i = 0; $i < random_int(5, 10); $i++) {
+            for($i = 0; $i < random_int(40, 100); $i++) {
                 $order = new Order;
                 $order->restaurant_id = $restaurant;
                 $order->customer_name = $faker->firstNameMale();
-                $order->customer_surname = $faker->firstNameFemale();
-                $order->customer_mail = $faker->email();
+                $order->customer_surname = $faker->lastName();
+                $order->customer_mail = $faker->safeEmail();
                 $order->customer_phone_number = $faker->numberBetween(3000000000, 3999999999);
                 $order->customer_address = $faker->streetAddress();
-                $order->total_amount = $faker->randomFloat(2, 0, 100);
+                $order->total_amount = $faker->randomFloat(2, 20, 100);
                 $order->status = $faker->boolean();
-                $order->note = $faker->paragraph();
                 $order->save();
             }
         }
